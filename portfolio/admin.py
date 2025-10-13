8daf13d4675998e27fd14dc28d84f413bdac2cd4
Luan Vieira <luanvwittchind@gmail.com>
2025-10-13 14:40:38 -0300

    Configura admin do app portfolio: permite criar postagens com autor automático

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from .models import Professor, Curso, Postagem
 
-admin.site.register(Professor)
-admin.site.register(Curso)
-admin.site.register(Postagem)
+# Admin para Professor
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'materia')
 
+# Admin para Curso
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'carga_horaria', 'professor')
 
+# Admin para Postagem
+@admin.register(Postagem)
+class PostagemAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'data_publicacao')
+    fields = ('titulo', 'conteudo')  # autor será preenchido automaticamente
+
+    # Define autor automaticamente ao criar a postagem
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # somente ao criar
+            obj.autor = request.user
+        super().save_model(request, obj, form, change)
